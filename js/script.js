6ab05999215fce46524b76eec033d2c92e521623
Safira Carvalho <sasantosgo17@gmail.com>
2025-12-29 21:45:26 -0300

    fixing the structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initAccordionList from "./modules/accordion.js";
+import initCarousel from "./modules/carousel.js";
 initSmoothScroll();
 initAccordionList();
+initCarousel();
